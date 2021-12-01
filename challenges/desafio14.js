1b30e7f9e442129adbed65b9381d2e767ead9cce
vimossi <vini_agg@hotmail.com>
2021-12-01 09:14:48 -0400

    implementa req 13 e 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  { "valoresNutricionais.2.percentual": { $gt: 20, $lt: 40 } },
+  {
+    $push: 
+      { tags: "contém sódio" },
+           
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, tags: 1, nome: 1 },
+);
