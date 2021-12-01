f2c7dea771d9fd990aab97ba8e4292d0e8f5ff36
vimossi <vini_agg@hotmail.com>
2021-12-01 08:38:23 -0400

    implementa req 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+  {},
+  { $push: { valoresNutricionais: {
+    $each: [],
+    $sort: { percentual: -1 },
+  } } },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, valoresNutricionais: 1, nome: 1 },
+);
